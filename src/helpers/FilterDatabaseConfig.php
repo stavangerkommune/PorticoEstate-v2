068394551817d58055445c08c4a2f7c47f2e6e8f
Sigurd Nes <sigurdne@online.no>
2024-04-18 09:07:22 +0200

    Remove duplicate

--- src/helpers/FilterDatabaseConfig.php
+++ src/helpers/FilterDatabaseConfig.php
@@ -2,12 +2,13 @@
 
 $rootDir = dirname(__DIR__, 2);
 
-if (is_file($rootDir . '/config/header.inc.php')) {
-	$settings = require $rootDir . '/config/header.inc.php';
+if (is_file($rootDir . '/config/header.inc.php'))
+{
 	$settings = require $rootDir . '/config/header.inc.php';
 	$phpgw_domain = $settings['phpgw_domain'];
-
-} else {
+}
+else
+{
 	return [];
 }
 
@@ -18,14 +19,19 @@ if (isset($_POST['login']))	// on login
 {
 	$login = $_POST['login'];
 	$_logindomain = \Sanitizer::get_var('logindomain', 'string', 'POST', $default_domain);
-	if (strstr($login, '#') === False) {
+	if (strstr($login, '#') === False)
+	{
 		$login .= '#' . $_logindomain;
 	}
 	list(, $user_domain) = explode('#', $login);
-} else if (\Sanitizer::get_var('domain', 'string', 'REQUEST', false)) {
+}
+else if (\Sanitizer::get_var('domain', 'string', 'REQUEST', false))
+{
 	// on "normal" pageview
-	if (!$user_domain = \Sanitizer::get_var('domain', 'string', 'GET', false)) {
-		if (!$user_domain = \Sanitizer::get_var('domain', 'string', 'POST', false)) {
+	if (!$user_domain = \Sanitizer::get_var('domain', 'string', 'GET', false))
+	{
+		if (!$user_domain = \Sanitizer::get_var('domain', 'string', 'POST', false))
+		{
 			$user_domain = \Sanitizer::get_var('domain', 'string', 'COOKIE', false);
 		}
 	}
@@ -38,27 +44,31 @@ else if (isset($_COOKIE['ConfigDomain']))
 {
 	$user_domain =	\Sanitizer::get_var('ConfigDomain', 'string', 'COOKIE', false);
 }
- else {
+else
+{
 	$user_domain = \Sanitizer::get_var('last_domain', 'string', 'COOKIE', false);
 }
 
 $db_server = [];
-if (isset($phpgw_domain[$user_domain])) {
+if (isset($phpgw_domain[$user_domain]))
+{
 	$db_server['db_host']			= $phpgw_domain[$user_domain]['db_host'];
 	$db_server['db_port']			= $phpgw_domain[$user_domain]['db_port'];
 	$db_server['db_name']			= $phpgw_domain[$user_domain]['db_name'];
 	$db_server['db_user']			= $phpgw_domain[$user_domain]['db_user'];
 	$db_server['db_pass']			= $phpgw_domain[$user_domain]['db_pass'];
 	$db_server['db_type']			= $phpgw_domain[$user_domain]['db_type'];
-  $db_server['domain']      = $user_domain;
-} else {
+	$db_server['domain']      = $user_domain;
+}
+else
+{
 	$db_server['db_host']			= $phpgw_domain[$default_domain]['db_host'];
 	$db_server['db_port']			= $phpgw_domain[$default_domain]['db_port'];
 	$db_server['db_name']			= $phpgw_domain[$default_domain]['db_name'];
 	$db_server['db_user']			= $phpgw_domain[$default_domain]['db_user'];
 	$db_server['db_pass']			= $phpgw_domain[$default_domain]['db_pass'];
 	$db_server['db_type']			= $phpgw_domain[$default_domain]['db_type'];
-  $db_server['domain']      = $default_domain;
+	$db_server['domain']      = $default_domain;
 }
 
 return $db_server;
