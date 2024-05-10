df593ff4606ca6adfd7f47644ca1c94e0bd43627
Sigurd Nes <sigurdne@online.no>
2024-05-10 11:17:21 +0200

    remove obsolete

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
-
-use App\helpers\DebugArray;
 use Slim\Factory\AppFactory;
 use DI\ContainerBuilder;
-use App\middleware\ApiKeyVerifier;
-use App\middleware\AccessVerifier;
-use App\providers\AclServiceProvider;
 use App\providers\DatabaseServiceProvider;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-//phpinfo();
-define('SRC_ROOT_PATH' , __DIR__ . '/src');
+define('SRC_ROOT_PATH', __DIR__ . '/src');
 
 define('ACL_READ', 1);
 define('ACL_ADD', 2);
@@ -23,26 +17,16 @@ define('ACL_CUSTOM_1', 64);
 define('ACL_CUSTOM_2', 128);
 define('ACL_CUSTOM_3', 256);
 
-//phpinfo();
 $containerBuilder = new ContainerBuilder();
-
-
-if (is_file(__DIR__ . '/config/header.inc.php')) {
-  //  $settings = include __DIR__ . '/config/header.inc.php';
-} 
 
 require_once SRC_ROOT_PATH . '/helpers/Translation.php';
 require_once SRC_ROOT_PATH . '/helpers/Sanitizer.php';
 require_once SRC_ROOT_PATH . '/helpers/DebugArray.php';
-// Add your settings to the container
 
+// Add your settings to the container
 $database_settings = require_once SRC_ROOT_PATH . '/helpers/FilterDatabaseConfig.php';
 
-//_debug_array($database_settings);
-
 $containerBuilder->addDefinitions(['settings' => ['db' => $database_settings]]);
-//(require __DIR__ . '/config/dependencies.php')($containerBuilder);
-
 
 // Build PHP-DI Container instance
 $container = $containerBuilder->build();
@@ -53,19 +37,11 @@ $app = AppFactory::create();
 
 // Register service providers
 $datbaseProvider = new DatabaseServiceProvider();
-//$aclProvider = new AclServiceProvider();
 
 $datbaseProvider->register($app);
 
-
-//phpinfo();
-
 //require all routes
 require_once __DIR__ . '/src/routes/RegisterRoutes.php';
-
-
-//App\helpers\DebugArray::debug($app);
-
 
 $displayErrorDetails = true; // Set to false in production
 $logErrors = true;
@@ -74,8 +50,6 @@ $errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, $logErrors, $l
 // Get default error handler and override it with your custom error handler
 $customErrorHandler = new \App\helpers\ErrorHandler($app->getResponseFactory());
 $errorMiddleware->setDefaultErrorHandler($customErrorHandler);
-
-
 
 // Run the Slim app
 $app->run();
