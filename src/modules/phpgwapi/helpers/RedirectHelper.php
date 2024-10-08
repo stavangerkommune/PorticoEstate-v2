5648d660f6c1fd3d114a88c6645a2e65d64c1a7e
Sigurd Nes <sigurdne@online.no>
2024-10-08 17:10:51 +0200

    redirect

--- src/modules/phpgwapi/helpers/RedirectHelper.php
+++ src/modules/phpgwapi/helpers/RedirectHelper.php
@@ -5,7 +5,8 @@ namespace App\modules\phpgwapi\helpers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\modules\phpgwapi\security\Sessions;
-
+use App\modules\phpgwapi\services\Settings;
+use HTML5;
 
 class RedirectHelper
 {
@@ -14,10 +15,9 @@ class RedirectHelper
 		if (!Sessions::getInstance()->verify())
 		{
 			//Echo "No access" to the user
-			$response->getBody()->write('No access');
-			return $response;
+			//		$response->getBody()->write('No access');
+			//		return $response;
 		}
-
 		if (isset($_GET['go']))
 		{
 			$go = htmlspecialchars_decode(urldecode($_GET['go']));
@@ -28,22 +28,27 @@ class RedirectHelper
 		}
 		else
 		{
-			$htmlContent = '<!DOCTYPE html>
-    <html lang="en">
+			$serverSettings = Settings::getInstance()->get('server');
+
+			$htmlContent = <<<HTML
+			<!DOCTYPE html>
+    <html lang="{$serverSettings['default_lang']}">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Invalid Link</title>
+        <title>{$serverSettings['system_name']} / Invalid Link</title>
     </head>
     <body>
         <h1>Invalid Link</h1>
         <p>The link you followed is invalid.</p>
-        <a href="/">Return to Main Page</a>
+        <a href="home/">Return to Main Page</a>
     </body>
-    </html>';
+    </html>
+HTML;
 
 			$response->getBody()->write($htmlContent);
-			return $response;
+			return $response
+				->withHeader('Content-Type', 'text/html');
 		}
 	}
 }
